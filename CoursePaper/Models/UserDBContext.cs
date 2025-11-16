9bd6a5162200aa987b53500d3b5099a99884ba8c
xXXTylerDurdenXXx <143999505+xXXTylerDurdenXXx@users.noreply.github.com>
2025-11-16 21:40:26 +0300

    Создал UserDBContext

---
+++ CoursePaper/Models/UserDBContext.cs
@@ -0,0 +1,10 @@
+﻿namespace CoursePaper.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    public class UserDBContext : DbContext
+    {
+        DbSet<User> _users;
+
+        public UserDBContext(DbContextOptions<UserDBContext> options) :  base(options) { }
+    }
+}
